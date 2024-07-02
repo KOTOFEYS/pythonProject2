8526854fd53088537a220dc92ba7d0f63b19c852
KOTOFEYS <makarov.k@mail.ru>
2024-07-02 22:49:36 +0300

    "Неизменяемые и изменяемые объекты. Кортежи и списки"

---
+++ homework5.py
@@ -0,0 +1,10 @@
+# immutable_var=1, 5, 'a' ,'true'
+# print(immutable_var)
+# immutable_var[0]=10
+# print(immutable_var)
+
+# Элементы кортежа являются неизменяемыми типами данных.
+
+immutable_list=[1, 2, 'a','b', 'space']
+immutable_list[0]=3
+print(immutable_list)
